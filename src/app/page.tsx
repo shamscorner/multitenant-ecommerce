ecdaea384dacac342c7db36716c697b71d18bc48
Shamim <hossains159@gmail.com>
2025-05-27 17:46:17 +0600

    feat: update layout and home page styles with DM Sans font and improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>Hello world</h1>
       <Button>Click me</Button>
     </div>
   );
