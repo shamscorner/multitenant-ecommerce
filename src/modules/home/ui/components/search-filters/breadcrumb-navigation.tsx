d995a7d0be9079a660d4008abf3319b4f6545f2c
Shamim <hossains159@gmail.com>
2025-06-02 10:51:19 +0600

    feat: implement breadcrumb navigation and enhance search filters with category and subcategory support

---
+++ src/modules/home/ui/components/search-filters/breadcrumb-navigation.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+
+interface Props {
+  activeCategorySlug?: string | null;
+  activeCategoryName?: string | null;
+  activeSubCategoryName?: string | null;
+}
+
+export const BreadcrumbNavigation = ({
+  activeCategorySlug,
+  activeCategoryName,
+  activeSubCategoryName,
+}: Props) => {
+  if(!activeCategoryName || activeCategorySlug === 'all') return null;
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList className="text-lg font-semibold">
+        {activeSubCategoryName ? (
+          <>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild className="underline">
+                <Link href={`/${activeCategorySlug}`}>{activeCategoryName}</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>/</BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbPage>
+                {activeSubCategoryName}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </>
+        ): (
+          <BreadcrumbItem>
+            <BreadcrumbPage>
+              {activeCategoryName}
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        )}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -1,22 +1,47 @@
 "use client";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
+import { DEFAULT_BG_COLOR } from "@/modules/home/constants";
 import { useTRPC } from "@/trpc/client";
 
+import { BreadcrumbNavigation } from "./breadcrumb-navigation";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
 
 export const SearchFilters = () => {
+  const params = useParams() as { category?: string; subcategory?: string };
+
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
 
+  const categoryParam = params.category as string | undefined;
+  const activeCategorySlug = categoryParam || 'all';
+
+  const activeCategoryData = data.find((category) => category.slug === activeCategorySlug);
+  const activeCategoryColor = activeCategoryData?.color || DEFAULT_BG_COLOR;
+  const activeCategoryName = activeCategoryData?.name || null;
+
+  const activeSubCategory = params.subcategory as string | undefined;
+  const activeSubCategoryName = activeCategoryData?.subcategories.find(
+    (subcategory) => subcategory.slug === activeSubCategory
+  )?.name || null;
+
   return (
-    <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
+    <div
+      className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
+      style={{ backgroundColor: activeCategoryColor }}
+    >
       <SearchInput />
       <div className="hidden lg:block">
         <Categories data={data} />
       </div>
+      <BreadcrumbNavigation
+        activeCategorySlug={activeCategorySlug}
+        activeCategoryName={activeCategoryName}
+        activeSubCategoryName={activeSubCategoryName}
+      />
     </div>
   );
 };
