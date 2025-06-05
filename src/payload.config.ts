0354db7002fbd088ad5610b9eb53dc76a2d5d971
Shamim <hossains159@gmail.com>
2025-06-05 10:57:38 +0600

    feat: implement access control for user management and add isSuperAdmin utility

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,6 +16,7 @@ import { Reviews } from "./collections/Reviews";
 import { Tags } from "./collections/Tags";
 import { Tenants } from "./collections/Tenants";
 import { Users } from "./collections/Users";
+import { isSuperAdmin } from "./lib/access";
 import { Config } from "./payload-types";
 
 const filename = fileURLToPath(import.meta.url);
@@ -42,7 +43,7 @@ export default buildConfig({
       tenantsArrayField: {
         includeDefaultField: false,
       },
-      userHasAccessToAllTenants: (user) => Boolean(user?.roles?.includes("super-admin"))
+      userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
     // storage-adapter-placeholder
   ],
